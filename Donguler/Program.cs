890ebf71129bb69844a2d8a0a81bb4ae08d4e52a
ahmet <ahmet@LAPTOP-NI64LQAT>
2021-10-19 16:36:05 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kurslar = new string[] { "Yazılım Geliştirme Kursu", "Programlamaya başlangıç için temel kurs", "Java", "Pyhton","C++",
+               "C#","Javascript" };
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("For döngüsü bitti. Foreach başlıyor.");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("Sayfa sonu - footer");
+        }
+    }
+}
---
+++ KampIntro/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //"kategoriEtiketi"=Değer turucu,alias
+
+
+
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOranı = 1.45;
+            bool sistemeGirisiYapmıisMi = false;
+            double dolarDun = 9.19;
+            double dolarBugun = 9.27;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış oku");
+            }
+
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış oku");
+            }
+
+            else
+            {
+                Console.WriteLine("Sabit oku");
+            }
+
+
+
+
+            if (sistemeGirisiYapmıisMi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarlar Butonu");
+            }
+
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
